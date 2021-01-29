f18341eb95b2fbc161f92406a955c3aa20ce7f79
sunga201 <sung9255@gmail.com>
2021-01-29 14:54:46 +0900

    Liner schedule 관련 API 추가.

--- src/main/java/com/mert/controller/shipController/LinerScheduleController.java
+++ src/main/java/com/mert/controller/shipController/LinerScheduleController.java
@@ -1,7 +1,23 @@
 package com.mert.controller.shipController;
 
+import com.mert.dto.shipDto.LinerScheduleDto;
+import com.mert.service.shipService.LinerScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class LinerScheduleController {
+
+    private final LinerScheduleService linerScheduleService;
+
+    @GetMapping("liner-schedule")
+    public List<LinerScheduleDto> getAllSchedule(){
+        return linerScheduleService.getAllSchedule();
+    }
 }
--- src/main/java/com/mert/service/shipService/LinerScheduleService.java
+++ src/main/java/com/mert/service/shipService/LinerScheduleService.java
@@ -1,7 +1,35 @@
 package com.mert.service.shipService;
 
+import com.mert.dto.shipDto.LinerScheduleDto;
+import com.mert.model.shipModel.LinerSchedule;
+import com.mert.repository.shipInfoRepository.LinerScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class LinerScheduleService {
+
+    private final LinerScheduleRepository linerScheduleRepository;
+
+    public List<LinerScheduleDto> getAllSchedule(){
+        List<LinerSchedule> list = linerScheduleRepository.findAll();
+        List<LinerScheduleDto> dtoList = new ArrayList<>();
+        list.forEach(linerSchedule -> {
+           dtoList.add(LinerScheduleDto.builder()
+                   .id(linerSchedule.getId())
+                   .calendarId(linerSchedule.getId().toString())
+                   .title(linerSchedule.getLinerCode())
+                   .category("time")
+                   .start(linerSchedule.getEta())
+                   .end(linerSchedule.getEtd())
+                   .build());
+        });
+        return dtoList;
+    }
+
 }
